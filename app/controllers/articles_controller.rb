f089951c9a804b0d657c8a845f8f8ac873fcbc42
Martino Fabbro <martino.fabbro@gmail.com>
2019-08-06 16:34:22 +0200

    ex3

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+require 'open-uri'
+require 'json'
+
+class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all.reverse_order
+  end
+
+   def show; end
+
+   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def parse
+    url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+    word_serialized = open(url).read
+    word = JSON.parse(word_serialized)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+require 'faker'
+require "open-uri"
+require "json"
+require "pry-byebug"
+Article.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +10,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+ word_serialized = open(url).read
+ word = JSON.parse(word_serialized)
+ word.take(20).each do |url|
+    url2 = "https://hacker-news.firebaseio.com/v0/item/#{url}.json?print=pretty"
+    word_serialized2 = open(url2).read
+    word2 = JSON.parse(word_serialized2)
+        # binding.pry
+    Article.create(title: word2["title"], content: word2["url"])
+  end
+
+# 10.times do
+# Article.create(title: Faker::CryptoCoin.coin_name, content: Faker::ChuckNorris.fact)
+# end
